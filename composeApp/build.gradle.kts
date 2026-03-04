9df7128836c9e92c49c8844111859cbc125e1c4c
agc173 <fonsigark@gmail.com>
2026-03-04 13:25:42 +0100

    Pin AndroidX deps to AGP 8.7.3-compatible versions

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -28,6 +28,16 @@ kotlin {
     
     sourceSets {
         androidMain.dependencies {
+            constraints {
+                implementation(libs.androidx.core)
+                implementation(libs.androidx.core.ktx)
+                implementation(libs.androidx.activity)
+                implementation(libs.androidx.activity.ktx)
+                implementation(libs.androidx.activity.compose)
+                implementation(libs.androidx.navigationevent.android)
+                implementation(libs.androidx.navigationevent.compose.android)
+            }
+
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.activity.compose)
             implementation(libs.ktor.client.okhttp)
