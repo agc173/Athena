5c08f1d680c8d202ca19597f90054d822232469e
agc173 <fonsigark@gmail.com>
2026-03-12 12:49:28 +0100

    Improve Tarot reading UI: mystical cards, typography hierarchy, glow and spacing

--- composeApp/src/commonMain/kotlin/com/agc/bwitch/ui/tarot/TarotScreen.kt
+++ composeApp/src/commonMain/kotlin/com/agc/bwitch/ui/tarot/TarotScreen.kt
@@ -23,7 +23,7 @@ import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.BorderStroke
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
