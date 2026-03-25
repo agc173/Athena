4d5422357c7b20de3a2aa5b64bc454a1de18d959
agc173 <fonsigark@gmail.com>
2026-03-25 17:07:19 +0100

    Rediseña PortalScreen con módulos activos y próximos

--- composeApp/src/commonMain/kotlin/com/agc/bwitch/ui/portal/PortalItemConfig.kt
+++ composeApp/src/commonMain/kotlin/com/agc/bwitch/ui/portal/PortalItemConfig.kt
@@ -5,6 +5,7 @@ import com.agc.bwitch.presentation.navigation.Destination
 data class PortalItemConfig(
     val title: String,
     val subtitle: String,
+    val symbol: String,
     val destination: Destination?,
-    val enabled: Boolean = true
+    val enabled: Boolean = true,
 )
