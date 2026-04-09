2f7a997476ec615265b2915e5c7d1319b163f725
agc173 <fonsigark@gmail.com>
2026-04-09 10:45:49 +0200

    docs(changelog): clarify i18n rollback as final 2026-04-09 state

--- composeApp/src/commonMain/kotlin/com/agc/bwitch/App.kt
+++ composeApp/src/commonMain/kotlin/com/agc/bwitch/App.kt
@@ -2,19 +2,29 @@ package com.agc.bwitch
 
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.agc.bwitch.localization.AppLocaleEnvironment
+import com.agc.bwitch.presentation.localization.AppLanguageViewModel
 import com.agc.bwitch.ui.theme.BWitchTheme
 import com.agc.bwitch.ui.theme.BWitchThemeTokens
+import org.koin.compose.koinInject
 
 @Composable
 fun App() {
+    val appLanguageVm: AppLanguageViewModel = koinInject()
+    val appLanguageState by appLanguageVm.uiState.collectAsState()
+
     BWitchTheme {
-        Surface(
-            modifier = Modifier,
-            color = BWitchThemeTokens.extras.screenBackground,
-        ) {
-            AppRoot()
+        AppLocaleEnvironment(languageCode = appLanguageState.currentLanguage.code) {
+            Surface(
+                modifier = Modifier,
+                color = BWitchThemeTokens.extras.screenBackground,
+            ) {
+                AppRoot()
+            }
         }
     }
 }
