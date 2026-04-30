b64fa1b8fb32676828ae8dbd5388e7fec7faa766
agc173 <fonsigark@gmail.com>
2026-04-30 10:48:13 +0200

    chore: bump versionCode to 7 and versionName to 1.0.6

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -100,8 +100,8 @@ android {
         applicationId = "com.agc.bwitch"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 7
+        versionName = "1.0.6"
     }
     buildFeatures {
         buildConfig = true
