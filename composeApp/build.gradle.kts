f1763176c57bffd92434e356589fb2dca477c352
agc173 <fonsigark@gmail.com>
2026-05-13 08:57:36 +0200

    update Play testing build configuration

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -100,8 +100,8 @@ android {
         applicationId = "com.agc.bwitch"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 10
+        versionName = "1.0.9"
     }
     buildFeatures {
         buildConfig = true
