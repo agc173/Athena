630411b60eff16a8e72eee7357b9ef2dbb89fd5e
agc173 <fonsigark@gmail.com>
2026-06-26 10:39:00 +0200

    Add birthplace inputs to natal chart UI

--- composeApp/src/androidMain/kotlin/com/agc/bwitch/ui/astrology/BasicNatalChartUiCalculator.android.kt
+++ composeApp/src/androidMain/kotlin/com/agc/bwitch/ui/astrology/BasicNatalChartUiCalculator.android.kt
@@ -2,11 +2,12 @@ package com.agc.bwitch.ui.astrology
 
 import com.agc.bwitch.data.astrology.natal.BasicNatalChartCalculator
 import com.agc.bwitch.domain.astrology.natal.BirthDateTimeUtc
+import com.agc.bwitch.domain.astrology.natal.BirthLocation
 import com.agc.bwitch.domain.astrology.natal.NatalChartResult
 
 actual object BasicNatalChartUiCalculator {
     private val calculator = BasicNatalChartCalculator()
 
-    actual fun calculate(birthDateTimeUtc: BirthDateTimeUtc): NatalChartResult =
-        calculator.calculate(birthDateTimeUtc)
+    actual fun calculate(birthDateTimeUtc: BirthDateTimeUtc, birthLocation: BirthLocation?): NatalChartResult =
+        calculator.calculate(birthDateTimeUtc, birthLocation)
 }
